4739fbf1e4c117d77b3c66328e33cbf1cf79bc4b
“JuanCamiloMontoya” <jua.montoya@udla.edu.co>
2020-09-13 21:45:06 -0500

    feat(certificates): add module

--- scenes/Private/Private.js
+++ scenes/Private/Private.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StatusBar, ImageBackground } from 'react-native';
-import { Container, Header, Body, Title } from 'native-base';
+import { StatusBar, ImageBackground, Platform } from 'react-native';
+import { Container, Header, Body, Title, View } from 'native-base';
 
 import { Route, withRouter, Switch, BackButton, Router, useHistory, MemoryRouter } from 'react-router-native';
 import Profile from './Profile/Profile';
@@ -11,6 +11,9 @@ import { wp } from '../../common/constants/_Mixins';
 import CourseList from './Home/CourseList/CourseList';
 import Course from './Home/CourseList/Course/Course';
 import headerBg from '../../assets/images/header-bg.jpg'
+import Exam from './Home/CourseList/Course/Exam/Exam';
+import Information from './Profile/Views/Information/Information';
+import Certificates from './Profile/Views/Certificates/Certificates';
 
 const Private = (props) => {
 
@@ -18,16 +21,20 @@ const Private = (props) => {
 
   return (
     <Container>
-      <StatusBar backgroundColor="#1F8209" />
+      <StatusBar backgroundColor="#DDFCE2" />
       <BackButton>
         <ImageBackground source={headerBg} style={{ flex: 1 }} resizeMode="cover" >
+          {Platform.OS == 'ios' && <View style={{ backgroundColor: 'transparent', height:wp(6) }} />}
           <Router history={history}>
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/home" component={Home} />
               <Route exact path="/home/course-list" component={CourseList} />
-              <Route exact path="/home/course" component={Course} />
+              <Route exact path="/home/course-list/course/:courseId" component={Course} />
+              <Route exact path="/home/course-list/course/:courseId/exam/:examId" component={Exam} />
               <Route exact path="/profile" component={Profile} />
+              <Route exact path="/profile/information" component={Information} />
+              <Route exact path="/profile/certificates" component={Certificates} />
               <Route exact path="/forum" component={Forum} />
             </Switch>
           </Router>
