8b4bcc0699c24d2a252d9b678af6b7a9d5477673
Juan Camilo Montoya Gonzalez <juancamilomontoya@MacBook-Pro-de-Juan.local>
2020-09-14 23:18:57 -0500

    refactor(profile): add information and certificate

--- scenes/Private/Profile/Profile.js
+++ scenes/Private/Profile/Profile.js
@@ -1,55 +1,90 @@
-import React, { useState, useRef, useLinking, useEffect } from 'react';
-import { ImageBackground, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useRef, useLinking, useEffect, Fragment } from 'react';
+import { ImageBackground, Image, TouchableOpacity, StatusBar } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, withRouter } from "react-router-native";
+import { Link, withRouter, useHistory } from "react-router-native";
 import { Container, Header, Content, Button, ListItem, Text, Icon, Left, Body, Right, Switch, View } from 'native-base';
-import { createForm } from 'rc-form';
 
-import i18n from '../../../i18n/i18n'
 import { auth as AuthActions } from '../../../services/Auth/AuthActions'
 import user_img from '../../../assets/images/user.jpg'
+import styles from './ProfileSyles'
+import { wp } from '../../../common/constants/_Mixins';
 
 const Profile = (props) => {
 
+  const { logout } = AuthActions
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const { profile } = useSelector(state => state.user)
   return (
-    <View
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        paddingHorizontal: 15,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        padding: 20
-      }}>
-      <Image source={user_img} style={{ width: 300, height: 300 }} />
-      <Text style={{ fontSize: 35, fontWeight: 'bold', marginBottom: '10%' }}>Yulder Adrian</Text>
-      <ListItem icon>
-        <Left>
-          <Button style={{ backgroundColor: "#777" }}>
-            <Icon active name="clipboard-text" type="MaterialCommunityIcons" />
-          </Button>
-        </Left>
-        <TouchableOpacity>
-          <Right>
-            <Text style={{ fontWeight: 'bold', color: '#777', fontSize: 20 }}>Mi información</Text>
-          </Right>
+    <Fragment>
+      <View style={styles.header__container}>
+        <Left style={{ flex: 1 }} />
+        <Text style={styles.header__title}>Perfil</Text>
+        <TouchableOpacity
+          style={{ flex: 1, alignItems: 'flex-end', paddingRight: wp(2) }}
+          onPress={() => { dispatch(logout()); history.push('/') }}
+        >
+          <Icon name="logout" type="MaterialCommunityIcons" />
         </TouchableOpacity>
-      </ListItem>
-      <ListItem icon>
-        <Left>
-          <Button style={{ backgroundColor: "#777" }}>
-            <Icon active name="clipboard-text" type="MaterialCommunityIcons" />
-          </Button>
-        </Left>
-        <TouchableOpacity>
-          <Right>
-            <Text style={{ fontWeight: 'bold', color: '#777', fontSize: 20 }}>Mis certificados</Text>
-          </Right>
-        </TouchableOpacity>
-      </ListItem>
-    </View>
+      </View>
+      <View style={styles.content}>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            paddingHorizontal: 15,
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            flex: 1,
+            padding: 20,
+            backgroundColor: '#E6F4EE'
+          }}
+        >
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              paddingHorizontal: 15,
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+              flex: 1,
+              padding: 20,
+              backgroundColor: '#E6F4EE'
+            }}>
+            <View>
+              <Image source={user_img} style={{ width: wp(70), height: wp(70), borderRadius: wp(35) }} />
+            </View>
+            <Text style={{ fontSize: 35, textAlign: 'center', fontWeight: 'bold', marginBottom: '10%' }}>{profile?.firstName} {profile?.lastName}</Text>
+            <ListItem icon>
+              <Left>
+                <Button style={{ backgroundColor: "#777" }}>
+                  <Icon active name="clipboard-text" type="MaterialCommunityIcons" />
+                </Button>
+              </Left>
+              <TouchableOpacity onPress={() => history.push('/profile/information')}>
+                <Right>
+                  <Text style={{ fontWeight: 'bold', color: '#777', fontSize: 20 }}>Mi información</Text>
+                </Right>
+              </TouchableOpacity>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Button style={{ backgroundColor: "#777" }}>
+                  <Icon active name="clipboard-text" type="MaterialCommunityIcons" />
+                </Button>
+              </Left>
+              <TouchableOpacity onPress={() => history.push('/profile/certificates')}>
+                <Right>
+                  <Text style={{ fontWeight: 'bold', color: '#777', fontSize: 20 }}>Mis certificados</Text>
+                </Right>
+              </TouchableOpacity>
+            </ListItem>
+          </View>
+        </View>
+      </View>
+    </Fragment>
   )
 }
 
-export default withRouter(Profile)
+export default Profile
